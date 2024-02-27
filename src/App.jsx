dba64a4f4782b39af49f0b839792fb4a8c93710d
Mingu Kim <mingufkim@gmail.com>
2024-02-27 09:38:28 +0900

    Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,19 @@ import Board from "./Board"
 import { useState } from "react"
 
 function App() {
-  const [xIsNext, setXIsNext] = useState(true)
   const [history, setHistory] = useState([Array(9).fill(null)])
   const [currentMove, setCurrentMove] = useState(0)
+  const xIsNext = currentMove % 2 === 0
   const currentSquares = history[currentMove]
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
     setHistory(nextHistory)
     setCurrentMove(nextHistory.length - 1)
-    setXIsNext(!xIsNext)
   }
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove)
-    setXIsNext(nextMove % 2 === 0) // even moves are X's turn and odd moves are O's turn
   }
 
   const moves = history.map((squares, move) => {
