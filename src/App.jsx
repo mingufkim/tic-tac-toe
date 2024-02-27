6c9f6163c5efb70c6ac43840ad043c9574d129c5
Mingu Kim <mingufkim@gmail.com>
2024-02-27 09:29:23 +0900

    Implementing time travel

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,19 @@ import { useState } from "react"
 function App() {
   const [xIsNext, setXIsNext] = useState(true)
   const [history, setHistory] = useState([Array(9).fill(null)])
-  const currentSquares = history[history.length - 1]
+  const [currentMove, setCurrentMove] = useState(0)
+  const currentSquares = history[currentMove]
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares])
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+    setHistory(nextHistory)
+    setCurrentMove(nextHistory.length - 1)
     setXIsNext(!xIsNext)
   }
 
   function jumpTo(nextMove) {
-    // TODO
+    setCurrentMove(nextMove)
+    setXIsNext(nextMove % 2 === 0) // even moves are X's turn and odd moves are O's turn
   }
 
   const moves = history.map((squares, move) => {
