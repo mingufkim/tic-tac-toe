dd5f1af05c6796b81a38d8458abcffdb45a06d68
Mingu Kim <mingufkim@gmail.com>
2024-02-27 12:26:48 +0900

    Refactor Board to use two loops to make the squares

--- src/Board.jsx
+++ src/Board.jsx
@@ -28,25 +28,28 @@ export default function Board({ xIsNext, squares, onPlay }) {
     onPlay(nextSquares)
   }
 
+  const makeSquares = () => {
+    let rows = []
+    for (let i = 0; i < 3; i++) {
+      let row = []
+      for (let j = 0; j < 3; j++) {
+        row.push(
+          <Square
+            key={i * 3 + j}
+            value={squares[i * 3 + j]}
+            onSquareClick={() => handleClick(i * 3 + j)}
+          />
+        )
+      }
+      rows.push(<div key={i}>{row}</div>)
+    }
+    return rows
+  }
+
   return (
     <div className="board">
       <div className="status">{status}</div>
-
-      <div>
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div>
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div>
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {makeSquares()}
     </div>
   )
 }
